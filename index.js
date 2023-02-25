7d1684cba252098474b87db31f9a3db11b4d8e45
pandi1813 <andrea.peter1813@gmail.com>
2023-02-25 10:42:15 +0000

    added if statement to prompt questions if user wants to add new engineer

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ let team = [];
 
 
 
-
+let newTeamMember;
 
 startProgram()
 async function startProgram(){
 
     // team.push(new Manager("Bob", 1, "test@test.com", 223))
-    let {managerName, managerId, managerEmail, officeNumber} = await inquirer
+    let {managerName, managerId, managerEmail, officeNumber, newEmployee} = await inquirer
     .prompt([
     {
         type: 'input',
@@ -47,30 +47,70 @@ async function startProgram(){
         name: 'officeNumber',
         message: "Enter team manager's office number",
       },
-    ])
-    await addTeamMember()
-    .then();
-
-      let htmlDoc = render(team)
-      
-  
-      let writeHTML = fs.writeFile(outputPath, htmlDoc, (err) => console.log("File created"));
-    
-    
-    
-    
-  }
-
-  async function addTeamMember() {
-    await inquirer.prompt([
       {
         type: 'list',
-        name: 'addTeamMember',
-        choices: ['Add an Engineer', 'Add an intern', 'Finish building the team'],
-        filter(val) {
-          return val.toLowerCase();
-        }
+        name: 'newEmployee',
+        choices: ['Add an Engineer', 'Add an Intern', 'Finish building the team'],
       },
 
     ])
+  
+    if (newEmployee === "Add an Engineer") {
+      console.log("adding engineer")
+      let {engineerName, engineerId, engineerEmail, engineerGithub} = await inquirer
+      .prompt([
+        {
+        type: 'input',
+        name: 'engineerName',
+        message: "Enter engineer's name",
+        },
+        {
+          type: 'input',
+          name: 'engineerId',
+          message: "Enter engineer's ID number",
+        },
+        {
+          type: 'input',
+          name: 'engineerEmail',
+          message: "Enter engineer's email address",
+        },
+        {
+          type: 'input',
+          name: 'engineerGithub',
+          message: "Enter engineer's Github username",
+        }
+      ])
+    }else if(newEmployee === "Add an Intern"){
+      console.log("adding intern")
+    }
+
+    // await addTeamMember()
+    // .then();
+    
+    let htmlDoc = render(team)
+    
+    
+    let writeHTML = fs.writeFile(outputPath, htmlDoc, (err) => console.log("File created"));
+    
+    
+    
+    
   }
+  
+  // function addTeamMember() {
+  //   newTeamMember = inquirer.prompt([
+  //     {
+  //       type: 'list',
+  //       name: 'newTeamMember',
+  //       choices: ['Add an Engineer', 'Add an intern', 'Finish building the team'],
+  //     },
+  //   ])
+    
+    
+  //   if (newTeamMember === "add an engineer") {
+  //     console.log("new engineer")
+  //   }
+    
+    // return newTeamMember
+    // console.log(newTeamMember)
+  // }
